e5d615112764ffe21b27584806f6efe3369ceb9c
Rennan Candido <rennan_sbarros@hotmail.com>
2021-08-19 00:32:24 -0300

    Implementation of the side menu

--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -1,5 +1,26 @@
 import styled from "styled-components";
 
-export const Container = styled.div`
-    // background-color: ${(props) => props.theme.colors.primary};
+export const Container = styled.aside`
+    background-color: ${(props) => props.theme.colors.background};
+    width: 360px;
+    height: 100vh;
+    overflow-y: auto;
 `;
+
+export const Search = styled.section`
+    display: flex;
+    flex-direction: column; 
+    background-color: #ffffff;
+    justify-content: center;
+    padding: 16px;
+`;
+
+export const Logo = styled.img`
+    margin-bottom: 15px;
+`;
+
+
+/*
+flex-direction: column; -> Um conteúdo abaixo do outro.
+justify-content -> Centralizar o conteúdo.
+*/
