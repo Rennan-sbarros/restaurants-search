a3a57175816e6033a5f8b300888ab452ae16c40b
Rennan Candido <rennan_sbarros@hotmail.com>
2021-08-19 22:19:36 -0300

    Implementation of the carousel of images restaurantes below the search field

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,18 +1,31 @@
 import React, { useState } from 'react';
+import Slider from "react-slick";
 import TextField, { Input } from '@material/react-text-field';
 import MaterialIcon from '@material/react-material-icon';
 
 import logo from '../../assets/logo.svg';
+import imgRestaurante from '../../assets/restaurante-fake.png';
+import { Card } from '../../components';
 
-import { Container , Search, Logo } from './styles';
+import { Container , Search, Logo, Wrapper, Map, CarouselTitle, Carousel } from './styles';
 
 const Home = () => {
     const [inputValue, setInputValue] = useState();
 
+    const settings = {
+        dots: false,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        adaptiveHeight: true, // Adapta a altura em relação ao conteúdo
+    };
+
     return(
-        <Container>
+        <Wrapper>
+            <Container>
             <Search>
-                <Logo className="logo" src={logo} alt="Logo do site" />
+                <Logo src={logo} alt="Logo do site" />
                 <TextField
                     label='Pesquisar Restaurantes'
                     outlined
@@ -22,8 +35,21 @@ const Home = () => {
                         value={inputValue}
                         onChange={(e) => setInputValue(e.target.value)} />
                 </TextField>
+                <CarouselTitle>
+                    Na sua Área
+                </CarouselTitle>
+                <Carousel {...settings}>
+                    <Card photo={imgRestaurante} title="Nome do Restaurante"/>
+                    <Card photo={imgRestaurante} title="Nome do Restaurante"/>
+                    <Card photo={imgRestaurante} title="Nome do Restaurante"/>
+                    <Card photo={imgRestaurante} title="Nome do Restaurante"/>
+                    <Card photo={imgRestaurante} title="Nome do Restaurante"/>
+                    <Card photo={imgRestaurante} title="Nome do Restaurante"/>
+                </Carousel>
             </Search>
         </Container>
+        <Map />
+        </Wrapper>      
     );
 };
     
